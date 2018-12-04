466691159c8b11c329d5b1a05079e8aaa5f2f0ab
Alexander Polanco <alex20080220@gmail.com>
2018-12-04 12:21:15 -0400

    Updates

---
+++ precache-manifest.93e71b5ced2930a2cdf475558a451383.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "e57dafcd096cabca3889",
+    "url": "/ReactGify/static/js/runtime~main.e57dafcd.js"
+  },
+  {
+    "revision": "67827a4ed2e684821e99",
+    "url": "/ReactGify/static/js/main.67827a4e.chunk.js"
+  },
+  {
+    "revision": "b5cd2db33a01e0537ccd",
+    "url": "/ReactGify/static/js/1.b5cd2db3.chunk.js"
+  },
+  {
+    "revision": "67827a4ed2e684821e99",
+    "url": "/ReactGify/static/css/main.547c5c4b.chunk.css"
+  },
+  {
+    "revision": "6c30dd3a9c67fe03f3339ad9af83f8b0",
+    "url": "/ReactGify/index.html"
+  }
+];
